6248da132bb5548a69e692f54c81ac8e4155a8da
peteman-pixel <petterugstad@gmail.com>
2026-07-03 10:07:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,24 +4,30 @@ NORPULS
 Application
 ==========================================================
 */
+import { renderSidebar } from "./components/sidebar.js";
+import { renderDashboard } from "./components/dashboard.js";
+
 import EnergyService from "./services/energy.js";
 import CurrencyService from "./services/currency.js";
-
-import { renderDashboard } from "./components/dashboard.js";
 
 async function start() {
 
     console.log("Starting Norpuls...");
 
     const energy = await EnergyService.refresh();
-
     const currency = await CurrencyService.refresh();
 
-    renderDashboard({
+    document.getElementById("sidebar").innerHTML =
+        renderSidebar();
 
+    document.getElementById("topbar").innerHTML = `
+        <h1>Norpuls</h1>
+        <div>🇳🇴 Norge Dashboard</div>
+    `;
+
+    renderDashboard({
         energy,
         currency
-
     });
 
 }
